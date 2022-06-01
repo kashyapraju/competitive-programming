7859337c491bc1fd8449b68f7cacfd9788c67fa9
Raj kashyap <98572611+kashyapraju@users.noreply.github.com>
2022-06-01 17:53:52 +0530

    Add files via upload

---
+++ kadanes.cpp
@@ -0,0 +1,34 @@
+// maximum subarray sum by kadane's algorithm
+#include<bits/stdc++.h>
+#include<climits>
+using namespace std;
+
+
+int main()
+{
+    int n;
+    cin>>n;
+  int a[n];
+    for(int i=0;i<n;i++)
+    {
+       cin>>a[i];
+    }
+      int currentsum=0;
+      int maxsum=INT_MIN;
+
+      for(int i=0;i<n;i++)
+      {
+          currentsum+=a[i];
+          if(currentsum<0)
+          {
+              currentsum=0;
+          }
+          maxsum=max(maxsum,currentsum);
+        
+      }
+
+      cout<<maxsum<<endl;
+
+
+    return 0;
+}
